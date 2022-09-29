8e4293cb88d7e19cd029a0751406dff831c2abc4
Matuydod <eratordo2001@gmail.com>
2022-09-29 14:48:41 +0700

    Lab2

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
+    res.json({ message: "Welcome to review contact application." });
 });
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
